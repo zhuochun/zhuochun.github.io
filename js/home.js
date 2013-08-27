970808b6557153315a190c0ff7b4264cc0f6c46d
Wang Zhuochun <stone1551@gmail.com>
2013-08-27 16:30:53 +0800

    small change in home intro

--- js/home.js
+++ js/home.js
@@ -1,3 +1,19 @@
 $(function() {
-    $('.dynamo').dynamo();
+    function sayaword() {
+        var hour = new Date().getHours();
+
+        return hour <=  6 ? "Hello" :
+               hour <= 12 ? "Good morning" :
+               hour <= 18 ? "Good afternoon" :
+               hour <= 20 ? "Good evening" : "Hi";
+    }
+
+    $('.greeting').html(sayaword());
+
+    $('.dynamo').dynamo({
+        delay: 1000,
+        center: true,
+        lines: [ 'listen', 'think aloud', 'learn fast',
+                 'code', 'photograph', 'write', 'run']
+    });
 });
