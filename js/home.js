cde344ee72f4ad9f6aef89ad05555d77f78823fc
Zhuochun <zhuochun@hotmail.com>
2026-02-07 15:23:32 +0800

    Add newsletter section to home page (#38)

--- js/home.js
+++ js/home.js
@@ -13,4 +13,65 @@ $(function() {
     $(".greeting").html(sayaword());
 
     $(".sidebar").addClass("sidebar-color-" + date.getDay());
+
+    function formatNewsletterDate(value) {
+        if (!value) {
+            return "";
+        }
+
+        var parsed = new Date(value);
+        if (Number.isNaN(parsed.getTime())) {
+            return "";
+        }
+
+        return parsed.toLocaleDateString("en-GB", {
+            day: "numeric",
+            month: "short",
+            year: "numeric"
+        });
+    }
+
+    function buildNewsletterItem(item) {
+        var title = item.title || "Untitled";
+        var link = item.canonical_url || item.url || (item.slug ? "https://bicrement.substack.com/p/" + item.slug : "https://bicrement.substack.com/");
+        var dateText = formatNewsletterDate(item.post_date || item.published_at || item.created_at);
+
+        var metaText = dateText ? "On " + dateText + " about newsletter" : "Newsletter";
+
+        return (
+            "<section class=\"post\">" +
+                "<header class=\"post-header\">" +
+                    "<h2 class=\"post-title\">" +
+                        "<a href=\"" + link + "\" title=\"" + title + "\">" + title + "</a>" +
+                    "</h2>" +
+                    "<p class=\"post-meta\">" + metaText + "</p>" +
+                "</header>" +
+            "</section>"
+        );
+    }
+
+    function loadNewsletterPosts() {
+        var container = $("#newsletter-posts");
+        if (!container.length) {
+            return;
+        }
+
+        fetch("https://bicrement.substack.com/api/v1/archive?limit=3")
+            .then(function(response) {
+                if (!response.ok) {
+                    throw new Error("Failed to load newsletter posts");
+                }
+                return response.json();
+            })
+            .then(function(data) {
+                var posts = Array.isArray(data) ? data : (data && data.posts ? data.posts : []);
+                var html = posts.slice(0, 3).map(buildNewsletterItem).join("");
+                container.html(html || "<p class=\"post-meta\">No newsletter posts available yet.</p>");
+            })
+            .catch(function() {
+                container.html("<p class=\"post-meta\">Unable to load newsletter posts right now.</p>");
+            });
+    }
+
+    loadNewsletterPosts();
 });
